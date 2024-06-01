503183a505daf41e0e9f0a343e9e0266d86e7160
Furaingu-Dainaso-daidai <daigo7camera@gmail.com>
2024-06-01 19:10:56 +0900

    Update get-photo.js

--- functions/get-photo.js
+++ functions/get-photo.js
@@ -1,6 +1,6 @@
 exports.handler = async (event) => {
     const photoDatabase = {
-        "1": "https://imgur.com/a/wF1wsS2",
+        "1": "https://example.com/photo1.jpg",
         "2": "https://example.com/photo2.jpg",
         // 他の写真番号とURLを追加
     };
